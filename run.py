d36fbb4496139ccaecdc6ce96b106a0f759d9e9b
Jeroen Vegter <jeroenvegter@hotmail.com>
2018-10-25 12:07:51 +0000

    formatted the messages and user input

--- run.py
+++ run.py
@@ -2,6 +2,10 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+messages = []
+
+def add_messages(username, message):
+    messages.append("{}: {}".format(username, message))
 
 @app.route('/')
 def index():
@@ -10,10 +14,12 @@ def index():
     
 @app.route('/<username>')
 def user(username):
-    return "Hi " + username
+    """Display chat messages"""
+    return "Welcome, {0}".format(username, messages)
     
 @app.route('/<username>/<message>')
 def send_message(username, message):
+    """Create a new message and redirect back to the chat page"""
     return "{0}: {1}".format(username, message)
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
